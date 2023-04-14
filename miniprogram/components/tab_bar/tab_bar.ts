214ce07f1fb49362733f0e3619545d7210863116
HikariLan <shaokeyibb@126.com>
2023-04-14 14:59:16 +0800

    refactor: tab bar style

--- miniprogram/components/tab_bar/tab_bar.ts
+++ miniprogram/components/tab_bar/tab_bar.ts
@@ -4,20 +4,58 @@ Component({
    * Component properties
    */
   properties: {
-
+    currentKey: {
+      type: String,
+    }
   },
 
   /**
    * Component initial data
    */
   data: {
-
+    tabData: [
+      {
+        key: "index",
+        title: "主页",
+        icon: "mdi-light:home",
+        path: "/pages/index/index",
+        color: "#000"
+      },
+      {
+        key: "variants",
+        title: "应用中心",
+        icon: "mdi-light:sitemap",
+        path: "/pages/variants/variants",
+        color: "#000"
+      },
+      {
+        key: "space",
+        title: "我的",
+        icon: "mdi-light:account",
+        path: "/pages/space/space",
+        color: "#000"
+      }
+    ]
   },
 
   /**
    * Component methods
    */
   methods: {
-
-  }
+    onHitTab(e: any) {
+      wx.redirectTo({
+        url: e.currentTarget.dataset.item.path
+      })
+    }
+  },
+  lifetimes: {
+    attached: function() {
+      this.setData({
+        tabData: this.data.tabData.map(it=>{
+          if(it.key===this.properties.currentKey)it.color = "#66CCFF"
+          return it
+        })
+      })
+    },
+  },
 })
--- miniprogram/types.ts
+++ miniprogram/types.ts
@@ -1,14 +1,21 @@
 type PersonalInfomation = {
-    "XH": string,
-    "XZBJ": string,
-    "XM": string,
-    "XJZTMC": string,
-    "XJZT": string,
-    "SZBJ": string |null,
-    "SYBJ": string,
-    "SZSY": string |null,
-    "YHTX": string |null,
-    "XB": string,
-    "JG": string | null,
-    "SZXY": string
+  "XH": string,
+  "XZBJ": string,
+  "XM": string,
+  "XJZTMC": string,
+  "XJZT": string,
+  "SZBJ": string | null,
+  "SYBJ": string,
+  "SZSY": string | null,
+  "YHTX": string | null,
+  "XB": string,
+  "JG": string | null,
+  "SZXY": string
+}
+
+type TabData = {
+  "key": string,
+  "title": string,
+  "path": string,
+  "icon": string
 }
