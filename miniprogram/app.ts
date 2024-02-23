de039a973cc9da29e9a8a91fd67fe6b7f8f4010e
HikariLan <hikarilan@minecraft.kim>
2024-02-23 21:47:06 +0800

    feat: welcome

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -1,6 +1,17 @@
 // app.ts
 App<IAppOption>({
   globalData: {},
-  onLaunch() {
+  async onLaunch() {
+    if (!wx.getStorageSync("read_welcome"))
+      await wx.showModal({
+        title: "欢迎使用“智慧仙财”小程序！",
+        content: "本小程序仍处于测试阶段，如您有任何问题或功能建议，请务必通过右上角的菜单的“反馈与投诉”栏目告诉我们！",
+        showCancel: false,
+        confirmText: "好"
+      })
+    wx.setStorage({
+      key: "read_welcome",
+      data: true
+    })
   }
 })
