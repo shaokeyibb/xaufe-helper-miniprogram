e0a95422f5f9ec75bea7f0f74129864e801698df
HikariLan <hikarilan@minecraft.kim>
2024-02-24 01:30:27 +0800

    feat: multi-page schedule result

--- miniprogram/pages/jwgl/schedule_new/result.ts
+++ miniprogram/pages/jwgl/schedule_new/result.ts
@@ -1,11 +1,17 @@
 // pages/jwgl/schedule_new/result.ts
-Page({
+Page<{
+  data: any,
+  mode: 0 | 1 | 2, // today | tomorrow | all
+  towards: number
+}, Record<string, any>>({
 
   /**
    * 页面的初始数据
    */
   data: {
     data: [],
+    mode: 0,
+    towards: 0
   },
 
   /**
@@ -25,5 +31,30 @@ Page({
     wx.setNavigationBarTitle({
       title: '学生课表查询结果'
     })
+  },
+
+  switchMode(event: any & (0 | 1 | 2)){
+    if(typeof event === "number"){
+      this.setData({mode: event as (0 | 1 | 2)})
+      return
+    }
+    this.setData({mode: event.currentTarget.dataset.mode})
+  },
+
+  onTouch(event: any){
+    switch(event.type){
+      case "touchstart": {
+        this.setData({towards: event.changedTouches[0].clientX});
+        break
+      }
+      case "touchend": {
+        const moved = event.changedTouches[0].clientX - this.data.towards
+        if(Math.abs(moved) < 100) return
+        moved > 0 
+        ? this.switchMode(Math.max(this.data.mode - 1, 0)) 
+        : this.switchMode(Math.min(this.data.mode + 1, 2))
+        break
+      }
+    }
   }
 })
